5324f67d233886831a57a9c77e9ae39c91336fb3
DaurenSan <sansizbai@gmail.com>
2018-12-12 13:07:02 +0600

    api_ready

--- employees/salesmen/models.py
+++ employees/salesmen/models.py
@@ -3,4 +3,3 @@ from django.db import models
 class Task(models.Model):
     task_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField()
-# Create your models here.
--- employees/salesmen/views.py
+++ employees/salesmen/views.py
@@ -1,6 +1,13 @@
-from django.http import HttpResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+##from rest_framework.renderers import JSONRenderer
+##from rest_framework.parsers import JSONParser
+from salesmen.models import Task
+from salesmen.serializers import TaskSerializer
 
+class TaskList(APIView):
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.") 
-# Create your views here.
+    def get(self, request, format=None):
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data)
